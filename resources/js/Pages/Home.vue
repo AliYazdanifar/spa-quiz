5d786a3a09ce568088f6d80c4cc99e4dfac9d127
ali yazdanifar <33312581+AliYazdanifar@users.noreply.github.com>
2024-09-01 16:57:26 +0430

    configs is ok

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <Layout>
+        <h1 class="text-2xl font-bold">خوش آمدید به آزمون گیر آنلاین</h1>
+    </Layout>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+};
+</script>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mx-auto p-4">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Layout',
+};
+</script>
